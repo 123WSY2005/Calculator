eae4e2ed5d4d650f58aba9e1deb73ac6ee58d409
siyu wang <1792064167@qq.com>
2026-05-26 17:25:30 +0800

    Finish

---
+++ 111WSY2005/Calculator/CalculatorUnitText/CalculatorUnitText.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Calculator/calculator.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CalculatorUnitText
+{
+	TEST_CLASS(CalculatorUnitText)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Calculator* calc = new Calculator();
+			string ret = calc->Solve("11+22");
+			Assert::AreEqual(ret, (string)"11+22=33");
+
+		}
+	};
+}
